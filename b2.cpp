e8644ab5bd87b4a7c821d34e31b7fd57d5cddfc9
MatheusBlanco <msallesblanco@gmail.com>
2019-02-05 18:26:35 -0200

    .

---
+++ b2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool is_valid(const string& expression){
+    static map<char, char> open {{')', '('}, {']', '['}, {'}', '{'}};
+    stack<char> s;
+
+    for(auto& c : expression){
+        switch(c){
+            case '(':
+            case '[':
+            case '{':
+                s.push(c);
+                break;
+            case ')':
+            case ']':
+            case '}':
+                if(s.empty() || s.top() != open[c]){
+                    return false;
+                }
+                s.pop();
+        }
+    }
+
+    return s.empty();
+}
+
+int main(){
+    int cases;
+    string expression;
+
+    cin >> cases;
+    while(cases >= 1 && cases <= 100){
+        for(size_t i = 0; i < cases + 1; i++){
+            getline(cin, expression, '\n');
+            if(i != 0){
+                cout << (is_valid(expression) ? "Compilou" : "Erro de compilacao") << endl;
+            }
+        }
+    }
+
+    return 0;
+}
